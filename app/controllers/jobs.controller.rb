94c10e77f32b4bc74745652ab66954559de082c0
krsgarland <97922634+krsgarland@users.noreply.github.com>
2023-01-30 13:39:46 -0600

    create GET request route for jobs

---
+++ app/controllers/jobs.controller.rb
@@ -0,0 +1,11 @@
+class JobsController < ApplicationController
+    def index
+        [
+            { company: "Stark Enterprises", position: "Ruby Developer", location: "St. Louis, MO" },
+            { company: "WayneEnterprises", position: "Ruby on Rails Engineer", location: "Kansas City, MO" },
+            { company: "OsCorp", position: "Full Stack Ruby Developer", location: "Springfield, MO" }, { company: "Hydra", position: "Ruby Engineer", location: "Columbia, MO" },
+            { company: "S.H.I.E.L.D.", position: "Ruby on Rails Developer", location: "St. Joseph, MO" },
+           ]
+
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'pages#jobs'
+  get '/jobs' to: 'pages#jobs'
 end
